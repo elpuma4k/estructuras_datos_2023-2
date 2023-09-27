9673ecc30a9550bd13a4a6cfe7779c98485c2399
DavidNinoGalvis <110640743+DavidNinoGalvis@users.noreply.github.com>
2023-09-26 19:28:47 -0500

    Add files via upload

--- tarea4/sort/givers/givers.cpp
+++ tarea4/sort/givers/givers.cpp
@@ -1,54 +1,90 @@
-/*
- * Curso: Estructura de Datos 2023-2
- * Estudiante: David Nino - 1114874514
+/* 
+ * Tema: Tarea 4 - Ejercicio Givers
+ * Asignatura: Estructura de Datos
  * Profesor: Carlos Alberto Ramirez
- * Fecha: 24 de Septiembre del 2023
- */
-
+ * Estudiante: David Nino Galvis
+ * Fecha: 26 de Septiembre del 2023
+ * "Parabellum"
+ * Complejidad del Algoritmo: O()
+*/
 #include<stdio.h>
 #include<string>
 #include<vector>
+#include<iostream>
 
-class People{
-  public:
-    People(string, int);
-    People(string);
-    string name;
-    int money;
-}
+using namespace std;
 
-People::People(string name, int money){
-  this->name = name;
-  this->money = money;
-}
 
-People::People(string name){
-  this->name = name;
-  this->money = 0;
-}
-
-int giveMoney(People p1, Peple p2){
-  
-}
-
-void imprimirVector(std::vector<int>& vector){
-  int n = 0;
+void imprimirVector(const vector<string>& vector){
+  int n;
   for(n = 0; n < vector.size(); n++){
-    printf("--> %s",vector[n]);
+    cout << vector[n] << endl;
   }
+}
+
+void imprimirVectorInt(const vector<int>& vector){
+  int n;
+  for(n = 0; n < vector.size(); n++){
+    cout << "-> " << vector[n] << endl;
+  }
+}
+
+int findPerson(const vector<string>& vector, string& nombre){
+  int n = 0, ans;
+  bool flag = true;
+  while(n < vector.size() && flag){
+    if(nombre == vector[n]){
+      flag = false;
+      ans = n;
+    } 
+    n++;
+  }
+  return ans;
 }
 
 void solve(){
-  int n;
+  int n, i, position, j, money = 0, partition = 0, modul = 0;
   string nombre;
-  char aux[12];
+  bool flag;
+  vector<string> personas;
+  vector<int> dinero;
+  personas.clear();
+  dinero.clear();
+  while(cin >> n){
   for(i = 0; i < n; i++){
-    scanf("%s", &aux);
-    nombre = string(aux);
-    Personaje nombre(nombre);
+    cin >> nombre;
+    personas.push_back(nombre);
+    dinero.push_back(0);
   }
-  while(n--){
-    
+  i = 0; 
+/* Termina la Lectura de Datos */
+  while(i < n){
+    cout << "Pasa por aqui" << endl;
+    flag = true;
+    /*Variables a reiniciar:modul,partition,money*/
+    cin >> nombre;
+    cin >> money;
+    cin >> j;
+    if(j == 0) flag = false;
+    else {
+    modul = money % j;
+    partition = (money-modul) / j;
+    position = findPerson(personas,nombre);
+    money -= modul;
+    dinero[position] -= money;}
+    /*Aqui Realizamos la division y regreso del residuo*/
+    while(j-- && flag){
+      cout << "Pasa por aqui tambien" << endl;
+      cin >> nombre;
+      position = findPerson(personas, nombre);
+      dinero[position] += partition;
+    }
+    i++;
+  }
+  for(i = 0; i < personas.size(); i++){
+    cout << personas[i] << " " << dinero[i] << endl;
+  }
+  cout << endl;
   }
 }
 
