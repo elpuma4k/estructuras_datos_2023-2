2d3957f301e49465715889fe53f81fb657fa78dd
Juan Tabares <124408501+elpuma4k@users.noreply.github.com>
2023-10-22 00:19:21 -0500

    Update DisperseMatrix.cpp

--- proyecto_final/version2_vector/DisperseMatrix.cpp
+++ proyecto_final/version2_vector/DisperseMatrix.cpp
@@ -1,17 +1,5 @@
 #include "DisperseMatrix.h"
 
-void DisperseMatrix::imprimirNotacion() {
-    cout << "+-------------------------+" << endl;
-    cout << "| Valor  | Fila  | Columna |" << endl;
-    cout << "+-------------------------+" << endl;
-
-    for (int i = 0; i < values.size(); i++) {
-        cout << "|   " << values[i] << "   |   " << rows[i] << "   |   " << columns[i] << "   |" << endl;
-    }
-    cout << "+-------------------------+" << endl;
-    cout << "La cantidad de columnas es: " << countColumns << " la cantidad de filas es: " << countRows << endl;
-}
-
 DisperseMatrix::DisperseMatrix(vector<vector<int>> &vec){
   int i, j;
   if(vec.size() > countRows) countRows = vec.size();
