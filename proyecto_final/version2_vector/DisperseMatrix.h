2f39a8931d4c74b93c61879eefe816954419bdcd
Juan Tabares <124408501+elpuma4k@users.noreply.github.com>
2023-10-22 00:17:44 -0500

    Update DisperseMatrix.h

--- proyecto_final/version2_vector/DisperseMatrix.h
+++ proyecto_final/version2_vector/DisperseMatrix.h
@@ -1,71 +1,50 @@
 /*
- * Autor: David Niño - Juan David Tabares
- * Fecha de creación: 18 de Octubre
- * Profesor: Carlos Ramirez Restrepo
- * Versión: 1.0
- * 
- * Archivo encabezado libreria TAD MatrixDisperse
- * Representación Vectores
- */
+Autores: David Niño - Juan David Tabares
+Clase asociada al TAD DisperseMatrix con formato coordenado.
+*/
 
 #ifndef __DISPERSEMATRIX_H
 #define __DISPERSEMATRIX_H
 
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-/************************
-* ESTRUCTURAS DE DATOS *
-************************/
 class DisperseMatrix{
-  private:
+
     vector<int> values;
     vector<int> rows;
     vector<int> columns;
     int countRows = 0, countColumns = 0;
 
   public:
-    /************************
-    * OPERACIONES DEL TAD *
-    ************************/
-
-    /* Constructoras */
+    
+    //Constructoras
     DisperseMatrix(vector<vector<int>> &vect);
     DisperseMatrix(DisperseMatrix &m);
 
-    /* Modificadoras */
-    void rebuild();
-    void printNotation();
-    void printMatrix();
-    /* Retorna el valor deseado */
-    int get(int &i, int &j);
-    /* Obtiene el vector de los Datos importantes */
-    vector<int> getRow(int &j);
-    /*
-    vector<int> getCol(int &i);
-    // == Se Retorna un vector Incluyendo los Ceros ==
-    vector<int> getDisperseRow(int &j);
-    vector<int> getDisperseCol(int &i);
-    // -- Aqui ya inicia lo complicado --
-    void assign(int &i, int &j);
-    // Operacion De Matrices 
-    void add(DisperseMatrix &m);
-    void productVector(const vector &vec);
-    // Imprimir La matrix Separados por el carac elegido
-    void printMatrix(char &caracter);
-    // Obtiene el numero maximo de una matriz
+    //Operaciones de consulta
+    int get(int i, int j);
+    vector<int> getRow(int j);
+    vector<int> getCol(int i);
+    vector<int> getDisperseRow(int j);
+    vector<int> getDisperseCol(int i);
     int getMax();
     DisperseMatrix getTranspose();
-    // Aqui me falta una operacion
 
-    // Sobrecarga de Operadores
-    // Si Existe una matriz mas pequeña se rellenará con ceros
+    //Operaciones modificadoras
+    void rebuild();
+    void assign(int i, int j);
+    void add(DisperseMatrix &m);
+    void productVector(const vector &vec);
+    void printMatrix(char &sep);
+    DisperseMatrix addMatrixList(vector<DisperseMatrix> &iM);
+
+    //Sobrecarga de operadores
     DisperseMatrix operator+(DisperseMatrix &m);
     DisperseMatrix operator*(DisperseMatrix &m);
     bool operator==(DisperseMatrix &m);
-    */
+    
 };
 
 #endif
